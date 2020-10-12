e9756c835d570a1de14930731c5834c470012f39
Yogesh Buradkar <yogesh.buradkar@gmail.com>
2020-10-12 22:24:52 +0100

    remove unused code
    Signed-off-by: Yogesh Buradkar <yogesh.buradkar@gmail.com>

--- app/src/main/java/com/yb/uadnd/matchcentre/di/AppModule.kt
+++ app/src/main/java/com/yb/uadnd/matchcentre/di/AppModule.kt
@@ -1,23 +1,15 @@
 package com.yb.uadnd.matchcentre.di
 
 import com.yb.uadnd.matchcentre.SimpleIdlingResource
-import com.yb.uadnd.matchcentre.data.remote.CommentaryService
-import com.yb.uadnd.matchcentre.data.remote.MatchService
-import com.yb.uadnd.matchcentre.data.remote.MatchesDataSource
-import com.yb.uadnd.matchcentre.data.repo.AppCommentaryRepository
 import com.yb.uadnd.matchcentre.data.repo.AppMatchCommentaryRepository
 import com.yb.uadnd.matchcentre.data.repo.AppMatchRepository
 import com.yb.uadnd.matchcentre.domain.MatchRepository
-import com.yb.uadnd.matchcentre.domain.repos.CommentaryRepository
 import com.yb.uadnd.matchcentre.domain.repos.MatchCommentaryRepository
 import dagger.Module
 import dagger.Provides
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -27,10 +19,6 @@ class AppModule {
     @Singleton
     @Provides
     fun provideMatchRepo(matchRepo: AppMatchRepository): MatchRepository = matchRepo
-
-    @Singleton
-    @Provides
-    fun provideCommentaryRepo(commRepo: AppCommentaryRepository): CommentaryRepository = commRepo
 
     @Singleton
     @Provides
